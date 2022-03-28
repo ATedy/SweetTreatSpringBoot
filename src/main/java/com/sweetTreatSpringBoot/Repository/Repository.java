b8822747d09ad819b7d097eeb89ddf3b3b413bf3
Amanuel <amanuel.tedros@capgemini.com>
2022-03-28 15:43:07 +0100

    lambok getter added to repo class

--- src/main/java/com/sweetTreatSpringBoot/Repository/Repository.java
+++ src/main/java/com/sweetTreatSpringBoot/Repository/Repository.java
@@ -9,14 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Repository {
-    static List<Courier>  couriers =  new ArrayList<>();
+   @Getter static List<Courier>  couriers =  new ArrayList<>();
     static{
         couriers.add(new Courier("1", "Bobby", 5, LocalTime.parse("09:00"),  LocalTime.parse("13:00"), true, 1.75));
         couriers.add( new Courier("2","Martin", 3,  LocalTime.parse("09:00"),  LocalTime.parse("17:00"), false , 1.5));
         couriers.add( new Courier("3","Geoff",5,  LocalTime.parse("10:00"),  LocalTime.parse("17:00"), true , 2.00));
     }
 
-    public static List<Courier> getCouriers() {
-        return couriers;
-    }
 }
