8fc3f9b1f8d6f57c5b7a154a6b9ebcd1ffe05ddb
Amanuel <amanuel.tedros@capgemini.com>
2022-03-28 15:26:53 +0100

    code commented

--- src/main/java/com/sweetTreatSpringBoot/Repository/Repository.java
+++ src/main/java/com/sweetTreatSpringBoot/Repository/Repository.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 public class Repository {
 
-    public static List<Courier> allCouriers(){
-       List<Courier> couriers =  new ArrayList<>();
-        couriers.add(new Courier("1", "Bobby", 5, LocalTime.parse("09:00"),  LocalTime.parse("13:00"), true, 1.75));
-        couriers.add( new Courier("2","Martin", 3,  LocalTime.parse("09:00"),  LocalTime.parse("17:00"), false , 1.5));
-        couriers.add( new Courier("3","Geoff",5,  LocalTime.parse("10:00"),  LocalTime.parse("17:00"), true , 2.00));
-
-    return couriers;
-    }
+//    public static List<Courier> allCouriers(){
+//////       List<Courier> couriers =  new ArrayList<>();
+//////        couriers.add(new Courier("1", "Bobby", 5, LocalTime.parse("09:00"),  LocalTime.parse("13:00"), true, 1.75));
+//////        couriers.add( new Courier("2","Martin", 3,  LocalTime.parse("09:00"),  LocalTime.parse("17:00"), false , 1.5));
+//////        couriers.add( new Courier("3","Geoff",5,  LocalTime.parse("10:00"),  LocalTime.parse("17:00"), true , 2.00));
+////
+////    return couriers;
+//    }
 }
