9ccf183f5cf3ae96b99678ddff8dc87f04b0879a
Amanuel <amanuel.tedros@capgemini.com>
2022-03-29 11:19:42 +0100

    excep handler method added

--- src/main/java/com/sweetTreatSpringBoot/controllers/CourierController.java
+++ src/main/java/com/sweetTreatSpringBoot/controllers/CourierController.java
@@ -1,8 +1,12 @@
 package com.sweetTreatSpringBoot.controllers;
 import com.sweetTreatSpringBoot.entity.Courier;
 import com.sweetTreatSpringBoot.entity.Order;
+import com.sweetTreatSpringBoot.exceptionHandling.CourierErrorResponse;
+import com.sweetTreatSpringBoot.exceptionHandling.CourierNotFoundException;
 import com.sweetTreatSpringBoot.services.CourierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,6 +43,12 @@ public class CourierController {
     // single courier route
     @GetMapping("/couriers/{id}")
     public Courier getOneCourier(@PathVariable("id") String id){
+
+        //  checks id against courier list size
+        if((Integer.parseInt(id) >= courierService.getAll().size()) || Integer.parseInt(id) < 0){
+            throw new CourierNotFoundException("Courier with id " + id + " not found. " +
+                    "Please enter numbers between 0 and " + courierService.getAll().size());
+        }
         return courierService.getOneCourier(id);
     }
 
@@ -48,5 +58,15 @@ public class CourierController {
         return  courierService.cheapestCourierSelector(order);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<CourierErrorResponse> handleException(CourierNotFoundException exc){
+        CourierErrorResponse error = new CourierErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 
 }
