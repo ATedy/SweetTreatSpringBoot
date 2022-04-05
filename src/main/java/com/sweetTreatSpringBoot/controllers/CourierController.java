2b3b1d81d09562cced5868312ee770480196a961
Amanuel <amanuel.tedros@capgemini.com>
2022-04-05 14:44:39 +0100

    exception handler code uncommented n used

--- src/main/java/com/sweetTreatSpringBoot/controllers/CourierController.java
+++ src/main/java/com/sweetTreatSpringBoot/controllers/CourierController.java
@@ -1,10 +1,15 @@
 package com.sweetTreatSpringBoot.controllers;
 import com.sweetTreatSpringBoot.entity.Courier;
+import com.sweetTreatSpringBoot.entity.Order;
+import com.sweetTreatSpringBoot.exceptionHandling.CourierErrorResponse;
+import com.sweetTreatSpringBoot.exceptionHandling.CourierNotFoundException;
 import com.sweetTreatSpringBoot.services.CourierService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -14,6 +19,7 @@ public class CourierController {
     @Autowired
     private CourierService courierService;
 
+    // new Courier route
     @PostMapping(path = "/couriers/newCourier")
     public void addCourier(@RequestBody Courier courier){
         courierService.addCourier(courier);
@@ -42,28 +48,28 @@ public class CourierController {
 
     }
 
-//    // cheapest route
-//    @GetMapping("/couriers/cheapest")
-//    public ResponseEntity<Courier> cheapestCourier(@RequestBody Order order){
-//        if(courierService.cheapestCourierSelector(order) != null){
-//           return  new ResponseEntity<Courier>(courierService.cheapestCourierSelector(order), HttpStatus.OK);
-//
-//        }
-//        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//    }
-//
-//
-//
-//    // exception handler method for courier
-//    @ExceptionHandler
-//    public ResponseEntity<CourierErrorResponse> handleException(CourierNotFoundException exc){
-//        CourierErrorResponse error = new CourierErrorResponse();
-//
-//        error.setStatus(HttpStatus.NOT_FOUND.value());
-//        error.setMessage(exc.getMessage());
-//        error.setTimeStamp(LocalTime.now());
-//
-//        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-//    }
+    // cheapest courier route
+    @GetMapping("/couriers/cheapest")
+    public ResponseEntity<Courier> cheapestCourier(@RequestBody Order order){
+        if(courierService.cheapestCourierSelector(order) != null){
+           return  new ResponseEntity<Courier>(courierService.cheapestCourierSelector(order), HttpStatus.OK);
+
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+
+    // exception handler method for courier
+    @ExceptionHandler
+    public ResponseEntity<CourierErrorResponse> handleException(CourierNotFoundException exc){
+        CourierErrorResponse error = new CourierErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(LocalTime.now());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 
 }
